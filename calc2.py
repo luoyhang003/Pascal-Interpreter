b36d7417896034867f489dabee617f97c53dfa6c
Jason Luo <luoyhang003@hotmail.com>
2015-12-01 14:53:18 +0800

    add multidigit and minus operation support

--- calc2.py
+++ calc2.py
@@ -1,16 +1,22 @@
-# Version 0.1
-# Time: Nov,30,2015
+# Version 0.2
+# Time: Dec,1,2015
 # Auther: Jason Luo
-# Content: An easy Interpreter that can only interpret a single digit plus another single digit
+# Content: An easy Interpreter that can only interpret a multidigit plus or minus another multidigit
+# 
+#*******************= New Features =********************
+# 12.01 Support mutidigit calculation                  *
+#       Support minus operation                        *
+#*******************************************************
+
 
 # Token Types:
 # EOF: End-Of-File
 
-INTEGER, PLUS, EOF = 'INTEGER', 'PLUS', 'EOF'
+INTEGER, PLUS, MINUS, EOF = 'INTEGER', 'PLUS', 'MINUS', 'EOF'
 
 class Token(object):
     def __init__(self, type, value):
-        # Token types: INTEGER, PLUS, EOF
+        # Token types: INTEGER, PLUS, MINUS, EOF
         self.type = type
         # Token value: 0,1,2,3,4,5,6,7,8,9,+,None
         self.value = value
@@ -22,6 +28,7 @@ class Token(object):
         For examle:
         Token(INTEGER, 3)
         Token(PLUS, '+')
+        Token(MINUS, '-')
         """
         return 'Token({type},{value})'.format(
                 type = self.type,
@@ -38,9 +45,34 @@ class Interpreter(object):
         self.text = text
         self.pos = 0
         self.current_token = None
+        self.current_char = self.text[self.pos]
 
     def error(self):
         raise Exception('Error Parsing Error!')
+
+    def advance(self):
+        # Advance the pos and set current_char
+        self.pos += 1
+
+        if self.pos > len(self.text)-1:
+            self.current_char = None
+        else:
+            self.current_char = self.text[self.pos]
+
+    def skip_whitespace(self):
+        # Skip space
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def integer(self):
+        # Support mutidigit integer
+        result = ''
+        
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        
+        return int(result)
 
     def get_next_token(self):
         """
@@ -52,29 +84,39 @@ class Interpreter(object):
         1. is pos past the end of the text?
         2. if so, return EOF
         3. get a character at pos, and decide its type depends on the single char
-        4. if it is a digit, then convert it to integer and return INTEGER token
-        5. if it is a '+', then return PLUS token
+        4. if it is a space, advance the pos
+        5. if it is a digit, then convert it to integer and return INTEGER token
+        6. if it is a '+', then return PLUS token
+        7. if it is a '-', then return MINUS token
         """
-        text = self.text
 
-        if self.pos > len(text) - 1:
-            return Token(EOF, None)
+        while self.current_char is not None:
 
-        current_char = text[self.pos]
+            if self.pos > len(self.text) - 1:
+                return Token(EOF, None)
 
-        if current_char.isdigit():
-            token = Token(INTEGER, int(current_char))
-            self.pos += 1
-            return token
+            current_char = self.text[self.pos]
 
-        if current_char == '+':
-            token = Token(PLUS, current_char)
-            self.pos += 1
-            return token
+            if current_char.isspace():
+                self.skip_whitespace()
+                continue
+
+            if current_char.isdigit():
+                return Token(INTEGER, self.integer())
+
+            if current_char == '+':
+                self.advance()
+                return Token(PLUS, '+')
+
+            if current_char == '-':
+                self.advance()
+                return Token(MINUS, '-')
+            
         
-        self.error()
+            self.error()
 
-        
+        return Token(EOF, None)
+
     def eat(self, token_type):
         # compare the current token with the passed token type
         # if they match then eat the current token and assign next token to the self.current_token
@@ -87,18 +129,27 @@ class Interpreter(object):
     
     def expr(self):
         # expr -> INTEGER PLUS INTEGER
+        # expr -> INTEGER MINUS INTEGER
+        
         self.current_token = self.get_next_token()
 
         left = self.current_token
         self.eat(INTEGER)
 
         op = self.current_token
-        self.eat(PLUS)
+
+        if op.type == PLUS:
+            self.eat(PLUS)
+        else:
+            self.eat(MINUS)
 
         right = self.current_token
         self.eat(INTEGER)
 
-        result = left.value + right.value
+        if op.type == PLUS:
+            result = left.value + right.value
+        else:
+            result = left.value - right.value
         return result
     
 def main():
